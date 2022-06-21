63211df95d62292f82d504aa96fbf79cfa0afc73
john0123456789 <johnluongpham@yahoo.com>
2022-06-21 13:07:58 -0700

    using useSelector to load articles directly from the reducer

--- src/components/SingleArticle/index.js
+++ src/components/SingleArticle/index.js
@@ -1,19 +1,18 @@
-import './SingleArticle.css';
+import "./SingleArticle.css";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
 const SingleArticle = () => {
+  const { id } = useParams();
+
+  const articles = useSelector((state) => state.articleState.entries);
+  const singleArt = articles.find((article) => article.id === id);
+
   return (
-    <div className='singleArticle'>
-      <h1>Why Am I At Home</h1>
-      <img
-        src='https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Frobcain%2Ffiles%2F2017%2F10%2FKevin-Home-Alone.jpg'
-        alt='home'
-      />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex esse
-        laboriosam officia accusantium veritatis fugiat exercitationem vero
-        autem nihil aliquid ullam recusandae, quis odit odio voluptates
-        explicabo nobis! Consequuntur, aliquam?
-      </p>
+    <div className="singleArticle">
+      <h1>{singleArt.title}</h1>
+      <img src={singleArt.imageUrl} alt="home" />
+      <p>{singleArt.body}</p>
     </div>
   );
 };
